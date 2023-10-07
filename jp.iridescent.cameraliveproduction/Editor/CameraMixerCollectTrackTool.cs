fcfc0c24f961fb1c69d418442bb8181e4ed78b47
pon <49616225+AJpon@users.noreply.github.com>
2023-10-07 20:18:50 +0900

    カスタムコントロールトラックに散らばったCameraMixerTimelineClipを一か所に集められるように

--- jp.iridescent.cameraliveproduction/Editor/CameraMixerCollectTrackTool.cs
+++ jp.iridescent.cameraliveproduction/Editor/CameraMixerCollectTrackTool.cs
@@ -9,8 +9,6 @@ using UnityEngine.Playables;
 using UnityEngine.Serialization;
 using UnityEngine.Timeline;
 using UnityEngine.UIElements;
-
-
 using Iridescent.Timeline;
 
 
@@ -75,8 +73,9 @@ public class CameraMixerCollectTrackTool : EditorWindow
 
         var renameCameraByClipNameButton = new Button();
 
-        renameCameraByClipNameButton.text = "Rename Camera By Clip Name";
+        renameCameraByClipNameButton.text = "Collect and Create new Track";
         renameCameraByClipNameButton.clicked += CollectAllClipsToNewTrack;
+        
 
         root.Add(objectField);
         root.Add(popupField);
@@ -107,64 +106,24 @@ public class CameraMixerCollectTrackTool : EditorWindow
             popupField.Clear();
         }
     }
+    
 
-
-    // 指定したTimelineAsset内のTimelineClipの内、asset に CameraMixerTimelineClip を持つものを時系列順に取得
-    // ControlTrack がある場合、その内部の PlayableAsset 内の CameraMixerTimelineClip も再帰的に取得する
-    public List<TimelineClip> GetCameraMixerTimelineClips(TimelineAsset timelineAsset)
+    // Scene内の全てのPlayableDirectorのうち、Binding対象に CameraMixerTimelineTrack を持つものを取得
+    public List<PlayableDirector> GetPlayableDirectors()
     {
-        Debug.Log("GetCameraMixerTimelineClips");
-        var result = new List<TimelineClip>();
-        foreach (var track in timelineAsset.GetOutputTracks())
+        var result = new List<PlayableDirector>();
+        var playableDirectors = FindObjectsOfType<PlayableDirector>(true);
+        foreach (var playableDirector in playableDirectors)
         {
-            result.AddRange(GetCameraMixerTimelineClips(track));
-        }
-
-        Debug.Log("GetCameraMixerTimelineClips End: " + result.Count + " clips found");
-        result.Sort((a, b) => a.start.CompareTo(b.start));
-        return result;
-    }
-
-    public List<TimelineClip> GetCameraMixerTimelineClips(TrackAsset trackAsset)
-    {
-        var result = new List<TimelineClip>();
-        if (trackAsset is CameraMixerTimelineTrack)
-        {
-            Debug.Log("GetCameraMixerTimelineClips: " + trackAsset.name + " is CameraMixerTimelineTrack");
-            var cameraSwitcherControlTrack = trackAsset as CameraMixerTimelineTrack;
-            cameraSwitcherControlTrack.muted = true;
-            result.AddRange(cameraSwitcherControlTrack.GetClips());
-        }
-        else if (trackAsset is ControlTrack)
-        {
-            // 入れ子になったTimelineAssetを再帰的に取得
-            Debug.Log("GetCameraMixerTimelineClips: " + trackAsset.name + " is ControlTrack");
-            foreach (var clip in trackAsset.GetClips())
+            var playableAsset = playableDirector.playableAsset as TimelineAsset;
+            if (playableAsset == null) continue;
+            foreach (var track in playableAsset.GetOutputTracks())
             {
-                var playableAsset = clip.asset as ControlPlayableAsset;
-                if (playableAsset == null) continue;
-                Debug.Log("Find ControlPlayableAsset: " + playableAsset.name);
-                // var timelinePlayable = playableAsset.sourceGameObject.Resolve(　
-
-                // var srcGameObject = playableAsset.sourceGameObject.Resolve()
-                //
-                //
-                // var timelineAsset2 = timelinePlayable as TimelineAsset;
-                // if (timelineAsset2 == null) continue;
-                // Debug.Log("!!Find nested timeline: " + timelineAsset2.name);
-                // result.AddRange(GetCameraMixerTimelineClips(timelineAsset2));
-            }
-        }
-        else
-        {
-            foreach (var clip in trackAsset.GetClips())
-            {
-                var playableAsset = clip.asset as PlayableAsset;
-                if (playableAsset == null) continue;
-                var timelineAsset2 = playableAsset as TimelineAsset;
-                if (timelineAsset2 == null) continue;
-                Debug.Log("??Find nested timeline: " + timelineAsset2.name);
-                result.AddRange(GetCameraMixerTimelineClips(timelineAsset2));
+                if (track is CameraMixerTimelineTrack)
+                {
+                    result.Add(playableDirector);
+                    break;
+                }
             }
         }
 
@@ -175,55 +134,103 @@ public class CameraMixerCollectTrackTool : EditorWindow
     // private List<TimelineClip> newClipList =　new List<TimelineClip>();
 
     [ContextMenu("Collect")]
-    public void CollectAllTimelineClips(CameraMixerTimelineTrack cameraSwitcherControlTrack)
+    public void CollectAllTimelineClips(CameraMixerTimelineTrack targetCameraSwitcherControlTrack)
     {
-        Debug.Log("CollectAllTimelineClips");
-        // newClipList.Clear();
         var timelineAsset = playableDirector.playableAsset as TimelineAsset;
         if (timelineAsset == null)
         {
-            Debug.LogWarning("TimelineAsset is null");
+            // Debug.LogWarning("TimelineAsset is null");
             return;
         }
 
         // cameraSwitcherControlTrack のクリップをすべて削除
-        foreach (var clip in cameraSwitcherControlTrack.GetClips())
+        foreach (var clip in targetCameraSwitcherControlTrack.GetClips())
         {
-            cameraSwitcherControlTrack.DeleteClip(clip);
+            targetCameraSwitcherControlTrack.DeleteClip(clip);
         }
 
         AssetDatabase.SaveAssets();
-        Debug.Log("Delete All Clips");
+        // Debug.Log("Delete All Clips");
+        var playableDirectors = GetPlayableDirectors();
 
-        // clip を複製して cameraSwitcherControlTrack に追加
-        foreach (var clip in GetCameraMixerTimelineClips(timelineAsset))
+        foreach (var track in timelineAsset.GetOutputTracks())
         {
-            Debug.Log(clip.start + ":" + clip.displayName);
+            // トラックが CustomControlTrack の場合
+            if (track is CustomControlTrack)
+            {
+                foreach (var clip in track.GetClips())
+                {
+                    // CustomControlClip の開始offset
+                    var startTimeOffset = clip.start - clip.clipIn;
+                    // CustomControlClip にセットされたゲームオブジェクトの取得
+                    var playableAsset = clip.asset as CustomControlPlayableAsset;
+                    if (playableAsset == null) continue;
+                    var gameObject = playableAsset.sourceGameObject.Resolve(playableDirector);
 
-            var newClip = cameraSwitcherControlTrack.CreateDefaultClip();
-            newClip.start = clip.start;
-            newClip.duration = clip.duration;
-            newClip.displayName = clip.displayName;
-            // var cameraMixerTimelineClip = clip.asset as CameraMixerTimelineClip;
-            // var newCameraMixerTimelineClip = newClip.asset as CameraMixerTimelineClip;
-            // newCameraMixerTimelineClip.camera = cameraMixerTimelineClip.camera;
-            // newCameraMixerTimelineClip.material = cameraMixerTimelineClip.material;
-            var newCameraMixerTimelineClip = Instantiate(clip.asset) as CameraMixerTimelineClip;
-            newClip.asset = newCameraMixerTimelineClip;
+                    foreach (var director in playableDirectors)
+                    {
+                        // CustomControlClip にセットされたゲームオブジェクトと同じゲームオブジェクトのPlayableDirectorを探す
+                        if (director.gameObject.Equals(gameObject) == false)
+                            continue;
 
-            EditorUtility.SetDirty(newClip.asset);
+                        // CustomControlClip にセットされたPlayableDirectorのTimelineAssetを取得
+                        var nestedTimelineAsset = director.playableAsset as TimelineAsset;
+                        if (nestedTimelineAsset == null)
+                            continue;
+
+                        // CustomControlClip にセットされたTimelineAssetのトラックを取得
+                        foreach (var nestedTrack in nestedTimelineAsset.GetOutputTracks())
+                        {
+                            if (nestedTrack is not CameraMixerTimelineTrack)
+                                continue;
+                            // Debug.Log("GetCameraMixerTimelineClips: " + nestedTrack.name +
+                            //           " is CameraMixerTimelineTrack");
+                            var cameraSwitcherControlTrack = nestedTrack as CameraMixerTimelineTrack;
+                            cameraSwitcherControlTrack.muted = true;
+
+                            // CustomControlClip 内の CameraSwitcherControlTrack のクリップを処理
+                            foreach (var cameraSwitcherClip in cameraSwitcherControlTrack.GetClips())
+                            {
+                                var newClip = targetTrack.CreateDefaultClip();
+                                newClip.displayName = cameraSwitcherClip.displayName;
+                                newClip.duration = cameraSwitcherClip.duration;
+                                newClip.start = cameraSwitcherClip.start + startTimeOffset;
+                                var clipAsset = newClip.asset as CameraMixerTimelineClip;
+                                var refAsset = cameraSwitcherClip.asset as CameraMixerTimelineClip;
+
+                                // Debug.Log(refAsset.liveCamera);
+
+                                // ここでカメラの参照を設定する
+
+                                var resolvedCamera = refAsset.camera.Resolve(director);
+                                // Debug.Log("resolvedCam: "+ resolvedCamera);
+                                    clipAsset.camera = new ExposedReference<LiveCamera>()
+                                {
+                                    defaultValue = resolvedCamera,
+                                    // exposedName = refAsset.camera.exposedName
+                                };
+
+                                clipAsset.material = refAsset.material;
+                                EditorUtility.SetDirty(newClip.asset);
+                            }
+                        }
+                    }
+                }
+            }
+
+            EditorUtility.SetDirty(timelineAsset);
+            AssetDatabase.SaveAssets();
+            AssetDatabase.Refresh();
         }
-
-        // 変更を保存
-        AssetDatabase.SaveAssets();
-
-        cameraSwitcherControlTrack.muted = false;
-        // UIを更新
     }
 
 
     [ContextMenu("Collect and Create new Track")]
     public void CollectAllClipsToNewTrack()
     {
+        var timelineAsset = playableDirector.playableAsset　as TimelineAsset;
+        // 新しいトラックを作成
+        var newTrack = timelineAsset.CreateTrack<CameraMixerTimelineTrack>(null, "CameraSwitcherControlTrack");
+        CollectAllTimelineClips(newTrack);
     }
 }
